d4c4f9380e260c5b72de276d6ffcdc428f637a90
luked <lcd67420@uga.edu>
2023-04-20 01:48:04 -0400

    wip width of binary tree

---
+++ problems/662-maximum_width_of_binary_tree/widthOfBinaryTree.c
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct node_and_pos {
+    struct TreeNode *node;
+    long pos;
+}
+int widthOfBinaryTree(struct TreeNode* root){
+
+} // wOBT()
+
+
