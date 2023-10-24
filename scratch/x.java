de1a7546c127148dec551dcf5edfcde17dea2aa1
lukecdean <ldean35@gatech.edu>
2023-10-23 23:50:38 -0400

    formatting

--- scratch/x.java
+++ scratch/x.java
@@ -2,13 +2,13 @@ class x {
     public static void main(String[] args) {
         char[] letters = {'a','e','o','u','c','m','n','r','s','v','w','x','z'};
         for (int i = 0; i < letters.length; i++) {
-        for (int ii = 0; ii < letters.length; ii++) {
-        for (int iii = 0; iii < letters.length; iii++) {
-            if (i == 9 || ii == 9 || iii == 9) {
-            System.out.printf("%c%c%c\n", letters[i], letters[ii], letters[iii]);
+            for (int ii = 0; ii < letters.length; ii++) {
+                for (int iii = 0; iii < letters.length; iii++) {
+                    if (i == 9 || ii == 9 || iii == 9) {
+                        System.out.printf("%c%c%c\n", letters[i], letters[ii], letters[iii]);
+                    }
+                }
             }
-        }
-        }
         } // for
     } // main()
 } // x
